65cdd28c079e09acebc5a7276fac0ebbfb23d9d4
james-eden0 <james.eden0@kakaoenterprise.com>
2021-05-11 18:26:32 +0900

    component develop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+function Food({fav}){
+  return <h3>I love {fav}</h3>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi"/>
     </div>
-  );
+  )
 }
 
 export default App;
